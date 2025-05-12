594bfd6ffd9e4fb79b053dde9468a10cda1cadbd
DONALD ASHWIN DSOUZA <42039542+ddonald17@users.noreply.github.com>
2025-05-12 14:59:04 +0000

    Add auto complete search bar

--- src/App.js
+++ src/App.js
@@ -1,100 +1,67 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
-import ProductCard from "./components/ProductCard";
 
 export default function App() {
-  const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const [totalProdInPage, setTotalProdInPage] = useState(10);
-
-  const totalPages = Math.ceil(products.length / totalProdInPage);
-
-  const pageNumbers = [];
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
-  }
-
-  const startIndex = (currentPage - 1) * totalProdInPage;
-
+  const [recipe, setRecipe] = useState([]);
+  const [input, setInput] = useState("");
+  const [showResult, setShowResult] = useState(false);
+  const [cache, setCache] = useState({});
+ 
+ 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    const timer = setTimeout(fetchRecipes, 300);
 
-  const fetchProducts = async () => {
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [input]);
+
+  const fetchRecipes = async () => {
     try {
-      const fetchedProducts = await fetch(
-        "https://dummyjson.com/products?limit=100"
-      );
-
-      if (!fetchedProducts.ok) {
-        throw new Error(`HTTP error! status: ${fetchedProducts.status}`);
+      if(cache[input]){
+        console.log("FROM CACHE")
+        setRecipe(cache[input]);
+        return;
       }
 
-      const data = await fetchedProducts.json();
-      setProducts(data.products);
+      console.log("API CALL");
+      const fetchedRecipes = await fetch(
+        `https://dummyjson.com/recipes/search?q=${input}`
+      );
+
+      if (!fetchedRecipes.ok) {
+        throw new Error(`HTTP error! status: ${fetchedRecipes.status}`);
+      }
+
+      const data = await fetchedRecipes.json();
+      setRecipe(data?.recipes);
+      setCache(prev => ({...prev, [input]: data?.recipes}));
     } catch (error) {
-      console.error("Failed to fetch products:", error.message);
+      console.error("Failed to fetch recipes:", error.message);
     }
   };
 
-  console.log(products);
-
-  if (!products.length) return <div>No Products found!!</div>;
+  console.log(input, "+++++");
 
   return (
     <div className="App">
       <div className="header">
-        <h1>Pagination</h1>
+        <h1>Autocomplete Search Bar</h1>
       </div>
-      <div className="product-list">
-        {products
-          .slice(startIndex, startIndex + totalProdInPage)
-          .map((prod, index) => (
-            <ProductCard key={index} img={prod.thumbnail} title={prod.title} />
-          ))}
-      </div>
-      <div className="pagination">
-        <button
-          key="prev"
-          onClick={() => setCurrentPage((prev) => prev - 1)}
-          className="prev"
-          disabled={currentPage === 1}
-        >
-          prev
-        </button>
-        {pageNumbers.map((number) => (
-          <button
-            key={number}
-            onClick={() => setCurrentPage(number)}
-            className={currentPage === number ? "active" : ""}
-          >
-            {number}
-          </button>
-        ))}
-        <button
-          key="next"
-          onClick={() => setCurrentPage((prev) => prev + 1)}
-          className="next"
-          disabled={currentPage === totalPages}
-        >
-          next
-        </button>
-        <div style={{ marginBottom: "1rem" }}>
-          <label htmlFor="itemsPerPage">Items per page: </label>
-          <select
-            id="itemsPerPage"
-            value={totalProdInPage}
-            onChange={(e) => {
-              setTotalProdInPage(Number(e.target.value));
-              setCurrentPage(1);
-            }}
-          >
-            <option value={5}>5</option>
-            <option value={10}>10</option>
-            <option value={15}>15</option>
-          </select>
-        </div>
+      <div className = "search-container">
+        <input
+          className = "search-input"
+          type = "text"
+          value = {input}
+          onChange = { (e) => setInput(e.target.value)}
+          onFocus = {() => setShowResult(true)}
+          onBlur = {() => setShowResult(false)}
+        />
+        {showResult && <div className = "result-container">
+        {recipe.map( (r) => (<div className="results" key = {r.id}>
+          {r.name}
+        </div>))}
+        </div>}
       </div>
     </div>
   );
