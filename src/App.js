efb1882f22fc0535a5b580b4c3f15cc7d2b88614
DONALD ASHWIN DSOUZA <42039542+ddonald17@users.noreply.github.com>
2025-05-14 16:45:36 +0000

    Add Progress Bar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
-import json from "./Data.json";
-import { addNode, deleteNode } from "./utils";
-import List from './components/List';
+import ProgressBar from "./components/ProgressBar";
 
 export default function App() {
-  const [data, setData] = useState(json);
-  
-  const handleAdd = (folderId) => {
-    const name = prompt("Enter name:");
-    const isFolder = window.confirm("Is it a folder?");
-    if (!name) return;
-
-    const newNode = {
-      id: Date.now(),
-      name,
-      isFolder,
-      ...(isFolder ? { children: [] } : {}),
-    };
-
-    setData((prev) => addNode(prev, folderId, newNode));
-  };
-
-  const handleDelete = (id) => {
-    if (window.confirm("Are you sure you want to delete this?")) {
-      setData((prev) => deleteNode(prev, id));
-    }
-  };
-
+  const progressValue = [10, 20, 30, 40, 80];
   return (
     <div className="App">
       <div className="header">
-        <h1>File Explorer</h1>
-      </div>
-      <div className="file-explorer">
-        <List list={data} handleAdd={handleAdd} handleDelete={handleDelete} />
+        <h1>Progress Bar</h1>
+        {progressValue.map((p) => (
+          <ProgressBar progress={p} />
+        ))}
       </div>
     </div>
   );
