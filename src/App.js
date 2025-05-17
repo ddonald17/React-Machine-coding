76ff8db49d8ce47f735e68a2311ded65d6e60893
DONALD ASHWIN DSOUZA <42039542+ddonald17@users.noreply.github.com>
2025-05-17 08:12:17 +0000

    Add OTP validation solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./styles.css";
-import ProgressBar from "./components/ProgressBar";
 
 export default function App() {
-  const progressValue = [10, 20, 30, 40, 80];
+  const OTP_SIZE = 5;
+  const [otpInput, setOtpInput] = useState(new Array(OTP_SIZE).fill(""));
+  const inputsRef = useRef([]);
+
+  const handleInputChange = (e, index) => {
+    const value = e.target.value;
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setOtpInput((prev) => {
+      const updated = [...prev];
+      updated[index] = value.slice(-1);
+      return updated;
+    });
+
+    if (value && index < OTP_SIZE - 1) {
+      inputsRef.current[index + 1].focus();
+    }
+  };
+
+  const handleBackSpace = (e, index) => {
+    if (e.key === "Backspace" && index > 0 && !otpInput[index]) {
+      inputsRef.current[index - 1].focus();
+      setOtpInput((prev) => {
+        const updated = [...prev];
+        updated[index] = "";
+        return updated;
+      });
+    }
+  };
   return (
     <div className="App">
       <div className="header">
-        <h1>Progress Bar</h1>
-        {progressValue.map((p) => (
-          <ProgressBar progress={p} />
+        <h1>OTP VALIDATION</h1>
+      </div>
+      <div className="otp-input">
+        {otpInput.map((otp, index) => (
+          <input
+            key={index}
+            type="text"
+            className="otp-input-box"
+            value={otp}
+            onChange={(e) => handleInputChange(e, index)}
+            ref={(el) => (inputsRef.current[index] = el)}
+            onKeyDown={(e) => handleBackSpace(e, index)}
+          />
         ))}
       </div>
     </div>
